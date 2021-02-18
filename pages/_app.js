8bbbd03bcc731828a775b6f592a3e66d656b2cb0
pedr0aroucha <pluccas500@gmail.com>
2021-02-18 18:10:53 -0300

    Meta Tags - SEO

--- pages/_app.js
+++ pages/_app.js
@@ -23,31 +23,48 @@ function MyApp({ Component, pageProps }) {
     <main>
       <TopProgressBar />
       <Head>
-        <meta httpEquiv="Content-Type" charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta
-          name="description"
-          content="Desenvolvedor de Software - Pedro Aroucha. Crio e desenvolvo soluções que facilitam sua vida. Pedro Lucas de Aroucha Monteiro - Anápolis Goiás."
-        />
-        <meta
-          name="copyright"
-          content="Feito por Pedro Lucas de Aroucha Monteiro"
-        />
-        <meta
-          name="keywords"
-          content="Programador, Web, sites, websites, Desenvolvedor,
-          Desenvolvedor Web, Programador JavaScript, 
-          Programador Python, Programador Web"
-        />
-        <meta
-          name="DOC.title"
-          content="Pedro Aroucha - Programador Web - Desenvolvedor de Software"
-        />
-        <title>Pedro Aroucha</title>
         <link rel="icon" href="/pedr0_dev.png" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
+        />
+
+        <meta charSet="utf-8" />
+        <meta name="language" content="pt-BR" />
+        <title>Pedro Aroucha - Desenvolvedor Web</title>
+        <meta
+          name="description"
+          content="Desenvolvo e crio soluções de forma rápida, simplificada, funcional e eficiente que facilitam sua vida !!!"
+        />
+        <meta name="robots" content="all" />
+        <meta name="author" content="Pedro Aroucha" />
+        <meta
+          name="keywords"
+          content="pedr0aroucha. programador, web, sites, sistemas web, SEO, API, integrações, Pedro Aroucha, desenvolvimento de software, códigos, algoritmos, ciência da computação, engenharia de software, engenharia da computação, programador web"
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://pedr0aroucha.dev.br" />
+        <meta property="og:title" content="Pedro Aroucha - Desenvolvedor Web" />
+        <meta
+          property="og:image"
+          content="https://github.com/pedr0aroucha.png"
+        />
+        <meta
+          property="og:description"
+          content="Desenvolvo e crio soluções de forma rápida, simplificada, funcional e eficiente que facilitam sua vida !!!"
+        />
+
+        <meta property="article:author" content="Pedro Aroucha" />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@pedr0aroucha" />
+        <meta name="twitter:title" content="pedr0aroucha" />
+        <meta name="twitter:creator" content="@pedr0aroucha" />
+        <meta
+          name="twitter:description"
+          content="Desenvolvo e crio soluções de forma rápida, simplificada, funcional e eficiente que facilitam sua vida !!!"
         />
       </Head>
       <Header />
